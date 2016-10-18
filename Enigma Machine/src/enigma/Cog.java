f511ff3d3f1dbe396733d0845f679d010984969d
JHowell45 <jhowell45@googlemail.com>
2016-10-18 14:23:01 +0100

    Filled out more of the Cogs class and created two get methods for retrieving information about the cogs. Also added javadocs for the respcetive methods.

--- Enigma Machine/src/enigma/Cog.java
+++ Enigma Machine/src/enigma/Cog.java
@@ -13,14 +13,39 @@ public class Cog {
 	 * All the private variables used in the Cog class
 	 * @param numberOfValues contains the number of potential values each cog can have, 
 	 *         will differ depending on whether it will include capitals or numbers.
+	 * @param sizeOfCog is used to decide the type of cog. There are three cogs in the enigma machine. 
+	 *         This means that they each take it in turn to rotate. Once the smallest cog 
+	 *         has done a full rotation the next cog does one rotation and they all follow the 
+	 *         same pattern.
 	 */
 	private int numberOfValues;
+	private String sizeOfCog;
 	
 	/**
-	 * 
-	 * @param numOfVals
+	 * This is a constructor for the Cog class and is used to create a new cog, giving the total 
+	 *         number of values and it's unique size out of 3.
+	 * @param numOfVals is the input given by the user to decide the umber of potential values the cog
+	 *         can have.
+	 * @param size is given by the user to choice what size the cog is given as a choice out of 3.
 	 */
-	public Cog(int numOfVals) {
-		numberOfValues = numOfVals;
+	public Cog(int numOfVals, String size) {
+		this.numberOfValues = numOfVals;
+		this.sizeOfCog = size;
+	}
+	
+	/**
+	 * A return method for getting the number of potential values for a specific cog.
+	 * @return The number of potential values the cog can have.
+	 */
+	public int getNumberOfValues() {
+	  return numberOfValues;
+	}
+	
+	/**
+	 * A return method for getting the size of the cog.
+	 * @return The size of the cog which should either be "Small", "Medium" or "Large".
+	 */
+	public String getSize() {
+	  return sizeOfCog;
 	}
 }
