b618b0cf0db366a56e2a99205dcd4bd934389f9b
JHowell45 <jhowell45@googlemail.com>
2016-10-19 21:32:04 +0100

    Created new Encryption class for holding all of the potential keys for encrypting/decrypting with and also the values that the cogs can hold.

---
+++ Enigma Machine/src/enigma/EncryptionKeys.java
@@ -0,0 +1,19 @@
+package enigma;
+
+public class EncryptionKeys {
+
+  /**
+   * 
+   */
+  public void encryptionKeys() {
+    // This method will hold all of the encryption keys used to encrypt/decrypt text.
+  }
+  
+  /**
+   * 
+   */
+  public void potentialValues() {
+    // This method will hold all of the potential values for the cogs.
+  }
+  
+}
