55eebaf48652c76a2cda81165f83de1487b0edfe
JHowell45 <jhowell45@googlemail.com>
2016-10-19 21:19:57 +0100

    Added cogRotate() method for rotating a cog after it encrypts a single character.

--- Enigma Machine/src/enigma/EnigmaMachine.java
+++ Enigma Machine/src/enigma/EnigmaMachine.java
@@ -24,16 +24,28 @@ public class EnigmaMachine {
   
   /**
    * This method is used to rotate the cogs after encrypting a character.
-   * @param sCog
-   * @param mCog
-   * @param lCog
-   * @param numOfVals
+   * @param sCog represents the small cog in the enigma machine.
+   * @param mCog represents the medium cog in the enigma machine.
+   * @param lCog represents the large cog in the enigma machine.
    */
-  public void cogRotate(Cog sCog, Cog mCog, Cog lCog, int numOfVals) {
+  public void cogRotate(Cog sCog, Cog mCog, Cog lCog) {
     if(sCog.getCurrentValue() < sCog.getNumberOfValues()) {
-      
+      int temp_small = sCog.getCurrentValue() + 1;
+      sCog.setCurrentValue(temp_small);
     } else {
-      
+      sCog.setCurrentValue(0);
+      if(mCog.getCurrentValue() < mCog.getNumberOfValues()) {
+        int temp_medium = mCog.getCurrentValue() + 1;
+        mCog.setCurrentValue(temp_medium);
+      } else {
+        mCog.setCurrentValue(0);
+        if(lCog.getCurrentValue() < lCog.getNumberOfValues()) {
+          int temp_large = lCog.getCurrentValue() + 1;
+          lCog.setCurrentValue(temp_large);
+        } else {
+          lCog.setCurrentValue(0);
+        }
+      }
     }
   }
 }
