6169e9d1c0ccab1909ba81f4ac162e2cd1c328b5
JHowell45 <jhowell45@googlemail.com>
2016-10-19 20:59:26 +0100

    Refactored the createCogs() method and added it to the new class EnigmaMachine and renamed the original class MainClass. Need new name for main class used ot run program.

--- Enigma Machine/src/enigma/EnigmaMachine.java
+++ Enigma Machine/src/enigma/EnigmaMachine.java
@@ -1,27 +1,12 @@
 /**
- * This is the main class which will be used for running the program on.
+ * This class is used for building the enigma machine to be used in the main class.
  * 
  * @author Jacob Howell
  */
 
 package enigma;
-import java.util.Scanner;
-
 
 public class EnigmaMachine {
-
-  /**
-   * Private variables used for storing key values for running the enigma program.
-   * 
-   * @param smallCog is used for holding the key information for the small cog.
-   * @param mediumCog is used for holding the key information for the medium cog.
-   * @param largeCog is used for holding the key information for the large cog.
-   */
-  private static Cog smallCog;
-  private static Cog mediumCog;
-  private static Cog largeCog;
-  private static String plainText;
-  private static String cipherText;
 
   /**
    * Method used for creating cogs to be used for the enigma machine.
@@ -31,28 +16,11 @@ public class EnigmaMachine {
    * @param mCog will hold the value of the middle cog.
    * @param lCog will hold the value of the largest cog.
    */
-  public static void createCogs(int numOfVals, Cog sCog, Cog mCog, Cog lCog) {
+  public void createCogs(int numOfVals, Cog sCog, Cog mCog, Cog lCog) {
     sCog = new Cog(numOfVals, "small");
     mCog = new Cog(numOfVals, "medium");
     lCog = new Cog(numOfVals, "large");
   }
   
-  /**
-   * This method is used to receive the user input to be used as plaintext for the enigma machine.
-   */
-  public static void getPlainText() {
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Please enter some ciphertext: ");
-    plainText = scan.nextLine();
-    scan.close();
-  }
   
-  
-  /**
-   * This is the main method used for running the main program.
-   */
-  public static void main(String args[]) {
-    int numberOfValues = 26;
-    createCogs(numberOfValues, smallCog, mediumCog, largeCog);
-  }
 }
---
+++ Enigma Machine/src/enigma/MainClass.java
@@ -0,0 +1,45 @@
+/**
+ * This is the main class which will be used for running the program on.
+ * 
+ * @author Jacob Howell
+ */
+
+package enigma;
+import java.util.Scanner;
+
+
+public class MainClass {
+
+  /**
+   * Private variables used for storing key values for running the enigma program.
+   * 
+   * @param smallCog is used for holding the key information for the small cog.
+   * @param mediumCog is used for holding the key information for the medium cog.
+   * @param largeCog is used for holding the key information for the large cog.
+   */
+  private static Cog smallCog;
+  private static Cog mediumCog;
+  private static Cog largeCog;
+  private static String plainText;
+  private static String cipherText;
+  
+  /**
+   * This method is used to receive the user input to be used as plaintext for the enigma machine.
+   */
+  public static void getPlainText() {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please enter some ciphertext: ");
+    plainText = scan.nextLine();
+    scan.close();
+  }
+  
+  
+  /**
+   * This is the main method used for running the main program.
+   */
+  public static void main(String args[]) {
+    EnigmaMachine eMachine = new EnigmaMachine();
+    int numberOfValues = 26;
+    eMachine.createCogs(numberOfValues, smallCog, mediumCog, largeCog);
+  }
+}
