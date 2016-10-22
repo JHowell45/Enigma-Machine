bfc91cf4cf5a355ffe1a70cd6e4627c16d3d5516
JHowell45 <jhowell45@googlemail.com>
2016-10-22 18:13:09 +0100

    remove original encryption and decryption methods and added ones for doing the same thing but for arrays. However, decryption doesnt work for any word after the first.

--- Enigma Machine/src/enigma/Encryption.java
+++ Enigma Machine/src/enigma/Encryption.java
@@ -10,8 +10,9 @@ package enigma;
 public class Encryption {
 
   /**
+   * Variables allowing the use of methods from the other classes.
    * 
-   * @param convert
+   * @param convert allows us to use methods from Conversion.
    */
   static Conversion convert = new Conversion();
 
@@ -21,7 +22,6 @@ public class Encryption {
    * @param c1 is the first, smallest, cog in the Enigma machine.
    * @param c2 is the second, middle-sized, cog in the Enigma machine.
    * @param c3 is the third, largest, cog in the Enigma machine.
-   * 
    * @return the key for the cogs.
    */
   public int getCogKey(Cog c1, Cog c2, Cog c3) {
@@ -40,94 +40,21 @@ public class Encryption {
   }
 
   /**
-   * This method is used to encrypt the plaintext and return the ciphertext.
+   * This method is used for encrypting every word in a plain text array.
    * 
-   * @param plainT is the plain text to be encrypted.
-   * @param valuesArray is the array of potential values the cogs can have.
+   * @param plainTArray plain text array to be encrypted.
+   * @param valuesArray is the array of values the cogs contain.
    * @param c1 is the first, smallest, cog.
    * @param c2 is the second, middle, cog.
    * @param c3 is the third, largest, cog.
-   * 
-   * @return the encrypted plain text (cipher text).
-   */
-  public String encryptPlainText(String plainT, char valuesArray[], Cog c1, Cog c2, Cog c3) {
-    // Empties the cipher String.
-    String cipherT = "";
-    // Convert the plain text into an array of characters.
-    char tempPlainArray[] = plainT.toCharArray();
-    // Create an array for the ciphertext that is the same length as the plain text array.
-    char tempCipherArray[] = new char[tempPlainArray.length];
-    int letterValue;
-    // A for loop for all of the letters in the plain text array.
-    for (int letter = 0; letter < tempPlainArray.length; letter++) {
-      letterValue = convert.convertToValue(valuesArray, tempPlainArray[letter]);
-      for (int encryptKey = 0; encryptKey < getCogKey(c1, c2, c3); encryptKey++) {
-        if (letterValue == c1.getNumberOfValues()) {
-          valuesArray[letterValue] = valuesArray[0];
-        } else {
-          valuesArray[letterValue] = valuesArray[letterValue++];
-        }
-      }
-      // Add the encrypted character to the cipher text array.
-      tempCipherArray[letter] = convert.convertToLetter(valuesArray, letterValue);
-      EnigmaMachine.cogRotate(c1, c2, c3);
-    }
-    // Convert the cipher text array to a string and assign it to the cipherT string.
-    cipherT = new String(tempCipherArray);
-
-    return cipherT;
-  }
-
-  /**
-   * this method is used to decrypt the cipher text into the plain text.
-   * 
-   * @param cipherT is the text created when encrypting the plain text.
-   * @param valuesArray is the array of potential values the cogs can have.
-   * @param c1 is the first, smallest, cog.
-   * @param c2 is the second, middle, cog.
-   * @param c3 is the third, largest, cog.
-   * 
-   * @return the decrypted cipher text (plain text).
-   */
-  public String decryptCipherText(String cipherT, char valuesArray[], Cog c1, Cog c2, Cog c3) {
-    // Empties the plain text String.
-    String plainT = "";
-    char tempCipherArray[] = cipherT.toCharArray();
-    char tempPlainArray[] = new char[tempCipherArray.length];
-    int encryptedLetterValue;
-    for (int encryptedLetter = 0; encryptedLetter < tempCipherArray.length; encryptedLetter++) {
-      encryptedLetterValue = convert.convertToValue(valuesArray, tempCipherArray[encryptedLetter]);
-      for (int encryptKey = 0; encryptKey < getCogKey(c1, c2, c3); encryptKey++) {
-        if (encryptedLetterValue == 0) {
-          valuesArray[encryptedLetterValue] = valuesArray[(c1.getNumberOfValues()) - 1];
-        } else {
-          valuesArray[encryptedLetterValue] = valuesArray[encryptedLetterValue--];
-        }
-      }
-      // Add the decrypted character to the plain text array.
-      tempPlainArray[encryptedLetter] = convert.convertToLetter(valuesArray, encryptedLetterValue);
-      EnigmaMachine.cogRotate(c1, c2, c3);
-    }
-    // Convert the plain text array to a string and assign it to the plainT string.
-    plainT = new String(tempPlainArray);
-
-    return plainT;
-  }
-
-  /**
-   * 
-   * @param plainTArray
-   * @param valuesArray
-   * @param c1
-   * @param c2
-   * @param c3
    * @return
    */
-  public String[] encryptPlainTextArray(String plainTArray[], char valuesArray[], Cog c1, Cog c2, Cog c3) {
+  public String[] encryptPlainTextArray(String plainTArray[], char valuesArray[], Cog c1, Cog c2,
+      Cog c3) {
     String cipherT = "";
     String cipherTextArray[] = new String[plainTArray.length];
-    for(String word : plainTArray) {
-      int counter = 0;
+    int counter = 0;
+    for (String word : plainTArray) {
       char tempPlainArray[] = word.toCharArray();
       // Create an array for the ciphertext that is the same length as the plain text array.
       char tempCipherArray[] = new char[tempPlainArray.length];
@@ -136,7 +63,7 @@ public class Encryption {
       for (int letter = 0; letter < tempPlainArray.length; letter++) {
         letterValue = convert.convertToValue(valuesArray, tempPlainArray[letter]);
         for (int encryptKey = 0; encryptKey < getCogKey(c1, c2, c3); encryptKey++) {
-          if (letterValue == c1.getNumberOfValues()-1) {
+          if (letterValue == c1.getNumberOfValues() - 1) {
             valuesArray[letterValue] = valuesArray[0];
           } else {
             valuesArray[letterValue] = valuesArray[letterValue++];
@@ -146,10 +73,54 @@ public class Encryption {
         tempCipherArray[letter] = convert.convertToLetter(valuesArray, letterValue);
         EnigmaMachine.cogRotate(c1, c2, c3);
       }
+      // Convert the cipher text array to a string and assign it to the cipherT string.
       cipherT = new String(tempCipherArray);
       cipherTextArray[counter] = cipherT;
       counter++;
     }
     return cipherTextArray;
   }
+
+  /**
+   * This method is used for decrypting every word in a cipher text array.
+   * 
+   * @param cipherTArray cipher text array to be decrypted.
+   * @param valuesArray is the array of values the cogs contain.
+   * @param c1 is the first, smallest, cog.
+   * @param c2 is the second, middle, cog.
+   * @param c3 is the third, largest, cog.
+   * @return
+   */
+  public String[] decryptionPlainTextArray(String cipherTArray[], char valuesArray[], Cog c1,
+      Cog c2, Cog c3) {
+    String plainT = "";
+    String plainTextArray[] = new String[cipherTArray.length];
+    int counter = 0;
+    for (String word : cipherTArray) {
+      char tempCipherArray[] = word.toCharArray();
+      char tempPlainArray[] = new char[tempCipherArray.length];
+      int encryptedLetterValue;
+      for (int encryptedLetter = 0; encryptedLetter < tempCipherArray.length; encryptedLetter++) {
+        encryptedLetterValue =
+            convert.convertToValue(valuesArray, tempCipherArray[encryptedLetter]);
+        for (int encryptKey = 0; encryptKey < getCogKey(c1, c2, c3); encryptKey++) {
+          if (encryptedLetterValue == 0) {
+            valuesArray[encryptedLetterValue] = valuesArray[(c1.getNumberOfValues()) - 1];
+          } else {
+            valuesArray[encryptedLetterValue] = valuesArray[encryptedLetterValue--];
+          }
+        }
+        // Add the decrypted character to the plain text array.
+        tempPlainArray[encryptedLetter] =
+            convert.convertToLetter(valuesArray, encryptedLetterValue);
+        EnigmaMachine.cogRotate(c1, c2, c3);
+      }
+      // Convert the plain text array to a string and assign it to the plainT string.
+      plainT = new String(tempPlainArray);
+      plainTextArray[counter] = plainT;
+      counter++;
+    }
+    return plainTextArray;
+  }
+
 }
--- Enigma Machine/src/enigma/MainClass.java
+++ Enigma Machine/src/enigma/MainClass.java
@@ -14,19 +14,26 @@ public class MainClass {
   /**
    * Private variables used for storing key values for running the enigma program.
    * 
-   * @param smallCog is used for holding the key information for the small cog.
-   * @param mediumCog is used for holding the key information for the medium cog.
-   * @param largeCog is used for holding the key information for the large cog.
+   * @param sCog is used for holding the key information for the small cog.
+   * @param mCog is used for holding the key information for the medium cog.
+   * @param lCog is used for holding the key information for the large cog.
    * @param plainText is used for storing the plain text to be encrypted.
+   * @param plainTextArray is used for storing an array of plain text, such as a sentence, to be
+   *        encrypted.
+   * @param cipherTextArray is used for storing an array of cipher text, such as a sentence, to be
+   *        decrypted.
    * @param cipherText is used for storing the encrypted plain text, which can be decryted back into
    *        plain text.
    * @param valuesLibrary is used for storing all of the character values of the cogs.
    */
+  private static Cog sCog;
+  private static Cog mCog;
+  private static Cog lCog;
   private static String plainText;
   private static String plainTextArray[];
   private static String cipherText;
   private static String cipherTextArray[];
-  private static char[] valuesLibrary;
+  private static char valuesLibrary[];
 
   /**
    * Variables allowing the use of methods from the other classes.
@@ -42,6 +49,18 @@ public class MainClass {
   static GetInput gInput = new GetInput();
 
   /**
+   * This method is used for printing String arrays as a sentence.
+   * 
+   * @param arr is the argument array to be printed.
+   */
+  public static void printStringArray(String[] arr) {
+    for (String s : arr) {
+      System.out.print(s + " ");
+    }
+    System.out.println();
+  }
+
+  /**
    * This is the main method used for running the main program.
    */
   public static void main(String args[]) {
@@ -50,38 +69,40 @@ public class MainClass {
     valuesLibrary = new char[numberOfValues];
     valuesLibrary = lib.potentialValues(numberOfValues);
     // Creating Cogs:
-    Cog sCog = new Cog(numberOfValues, 4, "small");
-    Cog mCog = new Cog(numberOfValues, 2, "medium");
-    Cog lCog = new Cog(numberOfValues, 0, "large");
-
+    sCog = new Cog(numberOfValues, 4, "small");
+    mCog = new Cog(numberOfValues, 2, "medium");
+    lCog = new Cog(numberOfValues, 0, "large");
+    // Creating original values for each Cog
     int sCogVal = sCog.getCurrentValue();
     int mCogVal = mCog.getCurrentValue();
     int lCogVal = lCog.getCurrentValue();
 
-    //plainText = gInput.getPlainText();
+    // plainText = gInput.getPlainText();
     plainText = "hello";
     plainTextArray = gInput.getPlainTextArray();
-    //System.out.println("The plain text is: " + plainText);
-    for(String s : plainTextArray) {
-      System.out.print(s + " ");
-    }
+    // System.out.println("The plain text is: " + plainText);
+    printStringArray(plainTextArray);
     System.out.println();
-    //System.out.println("The plain text array is: " + plainTextArray);
+    // System.out.println("The plain text array is: " + plainTextArray);
     int key = encrypt.getCogKey(sCog, mCog, lCog);
     System.out.println("The key is: " + key);
     System.out.println("The number of values each cog has is: " + sCog.getNumberOfValues());
-    //cipherText = encrypt.encryptPlainText(plainText, valuesLibrary, sCog, mCog, lCog);
-    //System.out.println("The plain text encrypted is: " + cipherText);
-    cipherTextArray = encrypt.encryptPlainTextArray(plainTextArray, valuesLibrary, sCog, mCog, lCog);
-    for(String s : cipherTextArray) {
-      System.out.print(s + " ");
-    }
+    // cipherText = encrypt.encryptPlainText(plainText, valuesLibrary, sCog, mCog, lCog);
+    // System.out.println("The plain text encrypted is: " + cipherText);
+    cipherTextArray =
+        encrypt.encryptPlainTextArray(plainTextArray, valuesLibrary, sCog, mCog, lCog);
+    printStringArray(cipherTextArray);
 
     sCog.setCurrentValue(sCogVal);
     mCog.setCurrentValue(mCogVal);
     lCog.setCurrentValue(lCogVal);
 
-    /*plainText = encrypt.decryptCipherText(cipherText, valuesLibrary, sCog, mCog, lCog);
-    System.out.println("The cipher text decrypted is: " + plainText);*/
+    plainTextArray =
+        encrypt.decryptionPlainTextArray(cipherTextArray, valuesLibrary, sCog, mCog, lCog);
+    printStringArray(plainTextArray);
+    /*
+     * plainText = encrypt.decryptCipherText(cipherText, valuesLibrary, sCog, mCog, lCog);
+     * System.out.println("The cipher text decrypted is: " + plainText);
+     */
   }
 }
