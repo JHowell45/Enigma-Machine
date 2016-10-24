35cfc7e9fda95fcc4d6ab92cdd811ad8ab3ce4b4
JHowell45 <jhowell45@googlemail.com>
2016-10-24 16:56:38 +0100

    Basic implementation of the first view the user will see.

--- Enigma Machine/src/UserInterface/MainWindow.java
+++ Enigma Machine/src/UserInterface/MainWindow.java
@@ -2,14 +2,19 @@ package UserInterface;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
 
 public class MainWindow extends JFrame {
 
   private JPanel contentPane;
+  private JTextField textField;
 
   /**
    * Launch the application.
@@ -31,12 +36,26 @@ public class MainWindow extends JFrame {
    * Create the frame.
    */
   public MainWindow() {
+    setTitle("MainView");
+    setResizable(false);
+    setAlwaysOnTop(true);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setBounds(100, 100, 450, 300);
+    setBounds(50, 50, 900, 600);
     contentPane = new JPanel();
     contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-    contentPane.setLayout(new BorderLayout(0, 0));
     setContentPane(contentPane);
-  }
+    contentPane.setLayout(null);
+    
+    JLabel viewTitle = new JLabel("Enigma Machine");
+    viewTitle.setFont(new Font("Lantinghei SC", Font.PLAIN, 24));
 
+    viewTitle.setHorizontalAlignment(SwingConstants.CENTER);
+    viewTitle.setBounds(6, 6, 888, 33);
+    contentPane.add(viewTitle);
+    
+    textField = new JTextField();
+    textField.setBounds(308, 163, 134, 28);
+    contentPane.add(textField);
+    textField.setColumns(10);
+  }
 }
