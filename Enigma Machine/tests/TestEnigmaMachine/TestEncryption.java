563afd7e8d6c9b17a900f3d012cb5df0106210b4
JHowell45 <jhowell45@googlemail.com>
2017-06-06 19:45:51 +0100

    Updated the tests.

--- Enigma Machine/tests/TestEnigmaMachine/TestEncryption.java
+++ Enigma Machine/tests/TestEnigmaMachine/TestEncryption.java
@@ -21,18 +21,22 @@ public class TestEncryption {
   /**
    * These are the variables to be used in the Test class.
    * 
-   * @param testEncryption
-   * @param testSCog
-   * @param testMCog
-   * @param testLCog
-   * @param testLib
-   * @param maxVals
+   * @param testEncryption is used to access the Encryption classes functions for testing.
+   * @param testLib is used to access the functions within the Library class.
+   * @param testSCog tests the smallest Cog in the Enigma Machine.
+   * @param testMCog tests the medium Cog in the Enigma Machine.
+   * @param testLCog tests the largest Cog in the Enigma Machine.
+   * @param testLibVals is an array used to contain all of the values within the Library of
+   *        potential values.
+   * @param testPlainText is the variable used as a test for the plaintext.
+   * @param testCipherText is the variable used as a test for the ciphertext.
+   * @param maxVals is the variable bused to show the maximul number of values.
    */
   private Encryption testEncrypt;
+  private Library testLib;
   private Cog testSCog;
   private Cog testMCog;
   private Cog testLCog;
-  private Library testLib;
   private char[] testLibVals;
   private String[] testPlainText;
   private String[] testCipherText;
@@ -73,18 +77,50 @@ public class TestEncryption {
     testSCog = null;
     testMCog = null;
     testLCog = null;
+    testLibVals = null;
     testPlainText = null;
+    testCipherText = null;
 
     assertNull(testEncrypt);
     assertNull(testLib);
     assertNull(testSCog);
     assertNull(testMCog);
     assertNull(testLCog);
+    assertNull(testLibVals);
     assertNull(testPlainText);
+    assertNull(testCipherText);
   }
 
   @Test
   public void testExistence() {}
+
+  /**
+   * This method is used for testing the getCogKey() method which takes all of the values of each of
+   * the Cogs and performs a calculation using those values to get a key value used in the
+   * encryption and decryption of the plaintext and ciphertext respectively.
+   */
+  @Test
+  public void testGetCogKey() {
+    int testSCogValue = 2;
+    int testMCogValue = 10;
+    int testLCogValue = 15;
+    testSCog.setCurrentValue(testSCogValue);
+    testMCog.setCurrentValue(testMCogValue);
+    testLCog.setCurrentValue(testLCogValue);
+    int trueKeyValue = testSCogValue + testMCogValue + testLCogValue;
+    int testKeyValue = testEncrypt.getCogKey(testSCog, testMCog, testLCog);
+    assertNotNull(testKeyValue);
+    assertEquals("The key generated using the method does not equal the expected value.",
+        testKeyValue, trueKeyValue);
+    testSCog.setCurrentValue(0);
+    testMCog.setCurrentValue(0);
+    testLCog.setCurrentValue(0);
+    trueKeyValue = 5;
+    testKeyValue = testEncrypt.getCogKey(testSCog, testMCog, testLCog);
+    assertNotNull(testKeyValue);
+    assertEquals("The key generated using the method does not equal the expected value.",
+        testKeyValue, trueKeyValue);
+  }
 
   /**
    * This method is used for testing the encryptPlainTextArray() method in the Encryption class.
